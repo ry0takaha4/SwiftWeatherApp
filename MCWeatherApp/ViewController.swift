c3f2b0592241abdf62423b90e21c9e48a2b718ea
ryo.takahashi <b152029@kca.ac.jp>
2018-03-09 19:55:41 +0900

    Initial Commit

---
+++ MCWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCWeatherApp
+//
+//  Created by AdminAir on 2018/03/09.
+//  Copyright © 2018年 ryotakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
